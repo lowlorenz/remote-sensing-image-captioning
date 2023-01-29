b6601ba69aeaee961ac1ac9b8557c59bbb04952c
lorenz <lorenz.hufe@web.de>
2023-01-29 14:06:57 +0100

    added text diversity

--- strategies.py
+++ strategies.py
@@ -1,10 +1,18 @@
 import torch
-from model import ImageCaptioningSystem
 from pathlib import Path
 import os
 from sklearn.cluster import KMeans
 import numpy as np
 import time
+from transformers import GPT2TokenizerFast, BertTokenizer, BertModel
+
+
+bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+bert = BertModel.from_pretrained("bert-base-uncased")
+
+gpt_tokenizer = GPT2TokenizerFast.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning"
+)
 
 
 def load_embeddings(
@@ -13,16 +21,45 @@ def load_embeddings(
     expected_num_files: int,
 ):
     if type == "image":
-        load_image_embeddings(path, expected_num_files)
+        return load_image_embeddings(path, expected_num_files)
     elif type == "text":
-        load_text_embeddings(path, expected_num_files)
+        return load_text_embeddings(path, expected_num_files)
 
 
 def load_text_embeddings(
     path: str,
     expected_num_files: int,
 ):
-    ...
+    while True:
+        predicted_tokens_files = os.listdir(path)
+        predicted_tokens_files = [
+            file
+            for file in predicted_tokens_files
+            if file.startswith("predicted_tokens")
+        ]
+
+        id_files = os.listdir(path)
+        id_files = [file for file in id_files if file.startswith("img_ids")]
+
+        if len(id_files) == expected_num_files:
+            break
+
+        time.sleep(0.1)
+
+    predicted_tokens = torch.cat(
+        [torch.load(Path(path, file)) for file in predicted_tokens_files], axis=0
+    )
+
+    hypothesis = gpt_tokenizer.batch_decode(predicted_tokens, skip_special_tokens=True)
+    with torch.no_grad():
+        tokens = bert_tokenizer(hypothesis, padding="longest").input_ids
+        tokens = torch.tensor(tokens)
+        embeddings = bert(tokens).pooler_output.shape
+
+    ids = torch.cat([torch.load(Path(path, file)) for file in id_files], axis=0)
+    ids = ids.detach().cpu().numpy()
+
+    return embeddings, ids
 
 
 def load_image_embeddings(
